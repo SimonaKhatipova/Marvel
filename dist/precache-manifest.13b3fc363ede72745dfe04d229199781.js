d17cc1cff0ae6385cebe968d48ef7196d561c55e
Simona Khatipova <44696785+SimonaKhatipova@users.noreply.github.com>
2021-12-05 15:51:46 +0300

    финальная сборка

---
+++ dist/precache-manifest.13b3fc363ede72745dfe04d229199781.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "854dd069584e082de235bbcdc795abf9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fef8393685e12a1bc9c8",
+    "url": "/js/app.23ae0144.js"
+  },
+  {
+    "revision": "a3873639a6223bac2cca",
+    "url": "/js/chunk-vendors.b53b4d9f.js"
+  },
+  {
+    "revision": "03b8ad800b22244878af354b0662e153",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
